46782a6acf04b6c8207567b0fe1d13f375fb1076
lixchen <18935047797@163.com>
2017-06-22 20:50:22 +0800

    .

--- test/main.js
+++ test/main.js
@@ -10,13 +10,13 @@ function Article(obj) {
         <a href="${this.link}">read me</a>
     `
 }
-Article.prototype.render = function() {
+Article.prototype.render = function () {
     var section = document.createElement('section');
     section.innerHTML = this.content;
     main.appendChild(section);
 };
 
-data.forEach(function(item, index, arr) {
+data.forEach(function (item, index, arr) {
     if (index < 10) {
         var article = new Article(item);
         article.render();
@@ -24,15 +24,15 @@ data.forEach(function(item, index, arr) {
 });
 var arrLike = main.children;
 var count = 0;
-next.onclick = function() {
-        main.innerHTML = '';
-        data.forEach(function(item, index, arr) {
-            if (index > 9 * count && index < 20 * count) {
-                var article = new Article(item);
-                article.render();
-            }
-        });
-        count++;
-    }
+next.onclick = function () {
+    count++;
+    main.innerHTML = '';
+    data.forEach(function (item, index, arr) {
+        if (index > 9 * count && index < 20 * count) {
+            var article = new Article(item);
+            article.render();
+        }
+    });
+}
     // 待解决问题  removeChild 一组
     //
