c51a77fa6b77fb073990005ed25a8356c5a7ec60
lixchen <18935047797@163.com>
2017-07-03 21:53:12 +0800

    .

--- doc/JavaScript加载和执行.md
+++ doc/JavaScript加载和执行.md
@@ -1,23 +1,32 @@
 # JavaScript 加载和执行
 
-<time>2017/07/01</time> 
+2017/07/01
 
 由于 JavaScript 的阻塞特性，大多数浏览器在执行 JavaScript 代码时, 不能同时作其他任何事情。所以一个页面在加载时，JavaScript 执行过程越久，浏览器等待响应时间越长。
 
-1、 `<script>`每出现一次，都会让页面的渲染和下载停下来等待脚本的解析和执行。由于脚本会阻塞页面和其他资源的下载，所以推荐将所有的`<script>`标签尽可能放在body的闭合标签`</body>`之前。
+## 1、 js文件位置
 
-2、 如果页面中存在多个 JavaScript 文件，页面加载过程中会产生多次 HTTP 请求，同样会带来额外的消耗。因此尽可能将多个js文件合并为单个，使用一个`<script>`标签加载。
+`<script>`每出现一次，都会让页面的渲染和下载停下来等待脚本的解析和执行。由于脚本会阻塞页面和其他资源的下载，所以推荐将所有的`<script>`标签尽可能放在body的闭合标签 `</body>` 之前。
 
-3、 无阻塞脚本，即在页面加载完成后才加载 JavaScript 代码。
-    
-    - `<script>`标签的`defer`属性，该属性用在有`src`属性的`<script>`标签上，表示该脚本会延迟到文档解析完成后才执行。(如果js脚本不会修改DOM，那么使用该属性可以更安全的延迟执行)
-    - `async`用于异步加载脚本，该属性对内联脚本没有作用(即没有src属性的脚本)
-    - 带有`defer`属性和`async`属性的脚本都采用并行下载，在下载过程中不会产生阻塞。
-    - 区别在与，defer 需要等待页面完成加载后执行，即DOM加载完成(onload事件触发前)，而 async 则会在文件加载完成后自动执行。
+## 2、 合并js文件
 
-4、 XMLHttpRequest 脚本注入
+如果页面中存在多个 JavaScript 文件，页面加载过程中会产生多次 HTTP 请求，同样会带来额外的消耗。因此尽可能将多个js文件合并为单个，使用一个 `<script>` 标签加载。
 
-```js
+## 3、 无阻塞脚本
+
+在页面加载完成后才加载 JavaScript 代码。
+
+1.  `<script>`标签的defer属性，该属性用在有src属性的`<script>`标签上，表示该脚本会延迟到文档解析完成后才执行。(如果js脚本不会修改DOM，那么使用该属性可以更安全的延迟执行)
+
+1.  async用于异步加载脚本，该属性对内联脚本没有作用(即没有src属性的脚本).
+
+1.  带有defer属性和async属性的脚本都采用并行下载，在下载过程中不会产生阻塞。
+
+1.  区别在与，defer 需要等待页面完成加载后执行，即DOM加载完成(onload事件触发前)，而 async 则会在文件加载完成后自动执行。
+
+## 4、 XMLHttpRequest 脚本注入
+
+``` js
 var xhr = new XHRHttpRequest();
 xhr.open('get', 'xxx.js', true);
 xhr.onreadystatechange = function(){
@@ -32,9 +41,9 @@ xhr.onreadystatechange = function(){
 xhr.send(null);
 ```
 
-使用这种方式加载的有点是，可以不用立即执行。 因为代码是`<script>`标签之外返回的，因此下载后不会自动执行。缺点是只能请求相同域中的文件，意味着 JavaScript 文件不能从 CDN 下载。
+-  使用这种方式加载的有点是，可以不用立即执行。 因为代码是`<script>`标签之外返回的，因此下载后不会自动执行。缺点是只能请求相同域中的文件，意味着 JavaScript 文件不能从 CDN 下载。
 
-5、 动态加载脚本(最通用的方式)
+## 5、 动态加载脚本(最通用的方式)
 
 ```js
 var script = document.createElement('script');
@@ -60,9 +69,9 @@ script.onreadystatechange = function(){
 ```
 
 
-
+封装
 ```js
-// 封装
+
 function loadScript(url, cb){
     var script = document.createElement('script');
     if (script.readyState){
@@ -82,11 +91,11 @@ function loadScript(url, cb){
 }
 ```
 
-6、将脚本放在样式表的`<link>`标签后，会等待样式表下载完成。因此建议不要将脚本放在`<link>`标签后面。
+## 6、将脚本放在样式表的`<link>`标签后，会等待样式表下载完成。因此建议不要将脚本放在`<link>`标签后面。
 
 ## 总结
 
-- 向页面中动态添加大量 JavaScript 文件时:
++ 向页面中动态添加大量 JavaScript 文件时:
 
     + 先添加动态加载所需代码(尽可能精简)
 
--- js/data.js
+++ js/data.js
@@ -1,137 +1,140 @@
 // markdown数据
 var data = [{
-    "title": "article27",
-    "time": "first article's timer",
+    "title": "JavaScript加载和执行",
+    "time": "2017/07/01",
     "type": "html",
-    "description": "this is article27",
-}, {
-    "title": "article26",
-    "time": "second article's timer",
-    "type": "javascript",
-    "description": "this is article26",
-}, {
-    "title": "article25",
-    "time": "second article's timer",
-    "type": "css",
-    "description": "this is article25",
-}, {
-    "title": "article24",
-    "time": "second article's timer",
-    "type": "html",
-    "description": "this is article24",
-}, {
-    "title": "article23",
-    "time": "second article's timer",
-    "type": "javascript",
-    "description": "this is article23",
-}, {
-    "title": "article22",
-    "time": "second article's timer",
-    "type": "css",
-    "description": "this is article22",
-}, {
-    "title": "article21",
-    "time": "second article's timer",
-    "type": "javascript",
-    "description": "this is article21",
-}, {
-    "title": "article20",
-    "time": "second article's timer",
-    "type": "html",
-    "description": "this is article20",
-}, {
-    "title": "article19",
-    "time": "second article's timer",
-    "type": "css",
-    "description": "this is article19",
-}, {
-    "title": "article18",
-    "time": "second article's timer",
-    "type": "html",
-    "description": "this is article18",
-}, {
-    "title": "article17",
-    "time": "second article's timer",
-    "type": "javascript",
-    "description": "this is article17",
-}, {
-    "title": "article16",
-    "time": "second article's timer",
-    "type": "css",
-    "description": "this is article16",
-}, {
-    "title": "article15",
-    "time": "second article's timer",
-    "type": "html",
-    "description": "this is article15",
-}, {
-    "title": "article14",
-    "time": "second article's timer",
-    "type": "javascript",
-    "description": "this is article14",
-}, {
-    "title": "article13",
-    "time": "second article's timer",
-    "type": "css",
-    "description": "this is article13",
-}, {
-    "title": "article12",
-    "time": "second article's timer",
-    "type": "html",
-    "description": "this is article12",
-}, {
-    "title": "article11",
-    "time": "second article's timer",
-    "type": "javascript",
-    "description": "this is article11",
-}, {
-    "title": "article10",
-    "time": "second article's timer",
-    "type": "css",
-    "description": "this is article10",
-}, {
-    "title": "article9",
-    "time": "second article's timer",
-    "type": "css",
-    "description": "this is article9",
-}, {
-    "title": "article8",
-    "time": "second article's timer",
-    "type": "css",
-    "description": "this is article8",
-}, {
-    "title": "article7",
-    "time": "second article's timer",
-    "type": "html",
-    "description": "this is article7",
-}, {
-    "title": "article6",
-    "time": "second article's timer",
-    "type": "css",
-    "description": "this is article6",
-}, {
-    "title": "article5",
-    "time": "second article's timer",
-    "type": "html",
-    "description": "this is article5",
-}, {
-    "title": "article4",
-    "time": "second article's timer",
-    "type": "html",
-    "description": "this is article4",
-}, {
-    "title": "article3",
-    "time": "second article's timer",
-    "type": "html",
-    "description": "this is article3",
-}, {
-    "title": "article2",
-    "time": "second article's timer",
-    "type": "html",
-    "description": "this is article2",
-}, {
-    "title": "article1",
-    "time": "second article's timer",
-    "type": "javascript",
-    "description": "this is article1",
+    "description": "this is article27"
 }];
+
+
+// , {
+//     "title": "article26",
+//     "time": "second article's timer",
+//     "type": "javascript",
+//     "description": "this is article26",
+// }, {
+//     "title": "article25",
+//     "time": "second article's timer",
+//     "type": "css",
+//     "description": "this is article25",
+// }, {
+//     "title": "article24",
+//     "time": "second article's timer",
+//     "type": "html",
+//     "description": "this is article24",
+// }, {
+//     "title": "article23",
+//     "time": "second article's timer",
+//     "type": "javascript",
+//     "description": "this is article23",
+// }, {
+//     "title": "article22",
+//     "time": "second article's timer",
+//     "type": "css",
+//     "description": "this is article22",
+// }, {
+//     "title": "article21",
+//     "time": "second article's timer",
+//     "type": "javascript",
+//     "description": "this is article21",
+// }, {
+//     "title": "article20",
+//     "time": "second article's timer",
+//     "type": "html",
+//     "description": "this is article20",
+// }, {
+//     "title": "article19",
+//     "time": "second article's timer",
+//     "type": "css",
+//     "description": "this is article19",
+// }, {
+//     "title": "article18",
+//     "time": "second article's timer",
+//     "type": "html",
+//     "description": "this is article18",
+// }, {
+//     "title": "article17",
+//     "time": "second article's timer",
+//     "type": "javascript",
+//     "description": "this is article17",
+// }, {
+//     "title": "article16",
+//     "time": "second article's timer",
+//     "type": "css",
+//     "description": "this is article16",
+// }, {
+//     "title": "article15",
+//     "time": "second article's timer",
+//     "type": "html",
+//     "description": "this is article15",
+// }, {
+//     "title": "article14",
+//     "time": "second article's timer",
+//     "type": "javascript",
+//     "description": "this is article14",
+// }, {
+//     "title": "article13",
+//     "time": "second article's timer",
+//     "type": "css",
+//     "description": "this is article13",
+// }, {
+//     "title": "article12",
+//     "time": "second article's timer",
+//     "type": "html",
+//     "description": "this is article12",
+// }, {
+//     "title": "article11",
+//     "time": "second article's timer",
+//     "type": "javascript",
+//     "description": "this is article11",
+// }, {
+//     "title": "article10",
+//     "time": "second article's timer",
+//     "type": "css",
+//     "description": "this is article10",
+// }, {
+//     "title": "article9",
+//     "time": "second article's timer",
+//     "type": "css",
+//     "description": "this is article9",
+// }, {
+//     "title": "article8",
+//     "time": "second article's timer",
+//     "type": "css",
+//     "description": "this is article8",
+// }, {
+//     "title": "article7",
+//     "time": "second article's timer",
+//     "type": "html",
+//     "description": "this is article7",
+// }, {
+//     "title": "article6",
+//     "time": "second article's timer",
+//     "type": "css",
+//     "description": "this is article6",
+// }, {
+//     "title": "article5",
+//     "time": "second article's timer",
+//     "type": "html",
+//     "description": "this is article5",
+// }, {
+//     "title": "article4",
+//     "time": "second article's timer",
+//     "type": "html",
+//     "description": "this is article4",
+// }, {
+//     "title": "article3",
+//     "time": "second article's timer",
+//     "type": "html",
+//     "description": "this is article3",
+// }, {
+//     "title": "article2",
+//     "time": "second article's timer",
+//     "type": "html",
+//     "description": "this is article2",
+// }, {
+//     "title": "article1",
+//     "time": "second article's timer",
+//     "type": "javascript",
+//     "description": "this is article1",
+// }
